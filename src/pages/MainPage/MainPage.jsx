caece7029848bea693a9818db8405ef167049983
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-01 12:35:36 +0900

    chore: 초기 환경세팅 완료

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { MainPageContainer } from './MainPage.style'
+
+export default function MainPage() {
+  return <MainPageContainer>테스트</MainPageContainer>
+}
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  #root {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    height: 100vh;
+    width: 100%;
+  }
+  body {
+    position: relative;
+    margin: 0 auto;
+    height: 100%;
+    min-height: 99vh;
+    display: flex;
+    flex-direction: column;
+    max-width: 100vw;
+    background-color: #F8F8F8;
+    border-color: black;
+    align-items: center;
+  }
+`
+
+export default GlobalStyle
