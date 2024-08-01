815cf510fa183aa8684861048bf7c30e36c6d8f3
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-01 12:51:06 +0900

    chore: 절대경로 관련 설정 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,12 +11,13 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': 'error', // Prettier 규칙을 ESLint 오류로 표시합니다.
   },
 }
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MainPageContainer } from './MainPage.style'
+import { MainPageContainer } from '@pages/MainPage/MainPage.style'
 
 export default function MainPage() {
   return <MainPageContainer>테스트</MainPageContainer>
