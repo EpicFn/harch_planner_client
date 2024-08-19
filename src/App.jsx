571ef15630d47942a94b43cb09b4aa103f500880
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-19 10:31:08 +0900

    feat: 추후 쓰일 라우팅 경로 초기 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import MainPage from '@pages/MainPage/MainPage'
 import GlobalStyle from '@styles/GlobalStyle'
+import CalendarPage from '@pages/Calendar/CalendarPage'
+import LibraryPage from '@pages/Library/LibraryPage'
+import DailyPlanPage from '@pages/DailyPlan/DailyPlanPage'
+import TodayRecord from '@pages/Record/TodayRecord/TodayRecord'
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <MainPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/library" element={<LibraryPage />} />
+          <Route path="/daily-plan" element={<DailyPlanPage />} />
+          <Route path="/today-record" element={<TodayRecord />} />
+        </Routes>
+      </Router>
     </>
   )
 }
