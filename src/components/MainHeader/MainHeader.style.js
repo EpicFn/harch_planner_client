7c01f81b2fe0baab867a8f93de355902f0aebc3a
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-25 15:01:17 +0900

    design: 메인 페이지 헤더에 로고 추가 및 퍼블리싱

---
+++ src/components/MainHeader/MainHeader.style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const MainHeaderContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 130px;
+  padding: 30px;
+  box-sizing: border-box;
+`
+export const Logo = styled.img`
+  height: 100%;
+  width: auto;
+  margin-left: 40px;
+`
