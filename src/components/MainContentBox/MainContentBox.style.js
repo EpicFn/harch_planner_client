73927c90a25d469499fd5cff60fdcc16d1a234f2
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-26 17:45:14 +0900

    design: ContentBox 높이 재 변경

--- src/components/MainContentBox/MainContentBox.style.js
+++ src/components/MainContentBox/MainContentBox.style.js
@@ -30,7 +30,7 @@ export const MenuTap = styled.li`
 `
 
 export const ContentBox = styled.div`
-  height: 850px;
+  height: 750px;
   flex-shrink: 0;
   border-radius: 0px 0px 12px 12px;
   border: 1px solid #dedede;
