ec59dcaf6a290caea6cbf9770a7eb95aea279acb
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-30 14:00:23 +0900

    feat: 전반적인 컴포넌트 구조 변경 및 달력 이동에 따른 월 렌더링 구현

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,51 +1,103 @@
 import {
   CalendarContainer,
+  Container,
   GlobalStyle,
   HeaderContainer,
   MonthLabel,
+  MonthLabelBox,
   MonthTitle,
+  SidebarContainer,
+  TaskItem,
+  WeekTaskContainer,
+  WeekTaskTitle,
 } from '@components/Calendar/Calendar.style'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import FullCalendar from '@fullcalendar/react'
+import { useEffect, useState } from 'react'
 
 export default function Calendar() {
+  const [events] = useState([
+    { title: '수능특강 국어 3지문 끝내기', date: '2024-08-01' },
+    { title: '과탐 전체 끝내기', date: '2024-08-07' },
+    { title: '수능형식 수학 시작', date: '2024-08-14' },
+    {
+      title: '수능특강 국어 남은 지문 마무리, 수학학원 등록',
+      date: '2024-08-21',
+    },
+    { title: '입시설명회, 캠퍼스 투어 다녀오기', date: '2024-08-28' },
+  ])
+
+  const today = new Date()
+  const [currentMonth, setCurrentMonth] = useState(null)
+
+  const handleMonthChange = (arg) => {
+    const newMonth = arg.view.currentStart.getMonth() + 1
+    setCurrentMonth(newMonth)
+  }
+
+  useEffect(() => {
+    // 페이지 로드시 현재 월로 상태 업데이트 해줬습니다
+    const initialMonth = today.getMonth() + 1
+    setCurrentMonth(initialMonth)
+  }, [])
+
   return (
     <>
       <GlobalStyle />
-      <CalendarContainer>
+      <Container>
         <HeaderContainer>
-          <MonthTitle>7</MonthTitle>
-          <MonthLabel>월간목표</MonthLabel>
+          <MonthTitle>{currentMonth}</MonthTitle>
+          <MonthLabelBox>
+            <MonthLabel>월간목표</MonthLabel>
+          </MonthLabelBox>
         </HeaderContainer>
-        <FullCalendar
-          plugins={[dayGridPlugin]}
-          initialView="dayGridMonth"
-          events={[
-            { title: '수능특강 국어 3지문 끝내기', date: '2024-08-01' },
-            { title: '과탐 전체 끝내기', date: '2024-08-07' },
-            { title: '수능형식 수학 시작', date: '2024-08-14' },
-            {
-              title: '수능특강 국어 남은 지문 마무리, 수학학원 등록',
-              date: '2024-08-21',
-            },
-            { title: '입시설명회, 캠퍼스 투어 다녀오기', date: '2024-08-28' },
-          ]}
-          locale="ko"
-          headerToolbar={{
-            left: 'prev,next today',
-            center: 'title',
-            right: 'dayGridMonth,dayGridWeek,dayGridDay',
-          }}
-          buttonText={{
-            today: '오늘',
-            month: '월간',
-            week: '주간',
-            day: '일간',
-          }}
-          height="auto"
-          contentHeight="auto"
-        />
-      </CalendarContainer>
+        <CalendarContainer>
+          <SidebarContainer>
+            <WeekTaskContainer>
+              <WeekTaskTitle>1주차</WeekTaskTitle>
+              <TaskItem>수능특강 국어 3지문 끝내기</TaskItem>
+            </WeekTaskContainer>
+            <WeekTaskContainer>
+              <WeekTaskTitle>2주차</WeekTaskTitle>
+              <TaskItem>과탐 한권 끝내기</TaskItem>
+              <TaskItem>모의고사 오답정리</TaskItem>
+            </WeekTaskContainer>
+            <WeekTaskContainer>
+              <WeekTaskTitle>3주차</WeekTaskTitle>
+              <TaskItem>수능형식 수학 시작</TaskItem>
+            </WeekTaskContainer>
+            <WeekTaskContainer>
+              <WeekTaskTitle>4주차</WeekTaskTitle>
+              <TaskItem>수능특강 국어 남은 지문 마무리</TaskItem>
+              <TaskItem>수학학원 등록</TaskItem>
+            </WeekTaskContainer>
+            <WeekTaskContainer>
+              <WeekTaskTitle>5주차</WeekTaskTitle>
+              <TaskItem>입시설명회</TaskItem>
+              <TaskItem>캠퍼스 투어 다녀오기</TaskItem>
+            </WeekTaskContainer>
+          </SidebarContainer>
+          <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            events={events}
+            locale="ko"
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: '',
+            }}
+            buttonText={{
+              today: '오늘',
+            }}
+            datesSet={handleMonthChange}
+            viewDidMount={handleMonthChange}
+            viewDidUpdate={handleMonthChange}
+            height="auto"
+            contentHeight="auto"
+          />
+        </CalendarContainer>
+      </Container>
     </>
   )
 }
--- src/components/Calendar/Calendar.style.js
+++ src/components/Calendar/Calendar.style.js
@@ -1,17 +1,64 @@
 import styled, { createGlobalStyle } from 'styled-components'
 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column; // 사이드바와 캘린더를 가로로 배치
+  justify-content: flex-start; // 시작 부분에 정렬
+  width: 100%;
+  height: 100%; // 전체 화면 높이에 맞추기
+  padding: 30px 80px;
+  box-sizing: border-box;
+  align-items: center;
+`
+
+export const SidebarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 430px;
+  height: 565px; // 부모 컨테이너와 같은 높이로 설정
+  background-color: #f9f9f9;
+  padding: 20px;
+  border: 1px solid #e0e0e0;
+  box-sizing: border-box;
+  margin-right: 20px;
+  border-radius: 8px;
+  flex-shrink: 0;
+  overflow-y: scroll;
+`
+
+export const WeekTaskContainer = styled.div`
+  width: 100%;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #e5e5ec;
+`
+
+export const WeekTaskTitle = styled.h3`
+  font-size: 18px;
+  color: #333;
+  margin-bottom: 10px;
+`
+
+export const TaskItem = styled.div`
+  font-size: 16px;
+  color: #666;
+  margin-bottom: 5px;
+  background-color: #e5e5ec;
+  padding: 8px;
+  border-radius: 4px;
+`
+
 export const CalendarContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-grow: 1;
   padding: 30px;
   background-color: #fff;
+  border: 1px solid #e0e0e0;
   border-radius: 12px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  width: 100%; // 캘린더 전체 너비를 100%로 설정
-  max-width: 1500px; // 최대 너비를 설정하여 깨짐 방지
-  height: 700px;
-  min-height: 714px;
-  margin: 0 auto;
+  height: calc(100% - 80px); // 부모 컨테이너와 같은 높이로 설정
   box-sizing: border-box;
-  margin-top: 20px;
 `
 
 export const HeaderContainer = styled.div`
@@ -27,13 +74,24 @@ export const MonthTitle = styled.h1`
   margin: 0;
 `
 
+export const MonthLabelBox = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 76px;
+  margin-left: 15px;
+  border: 1px solid #cacad7;
+  border-radius: 8px;
+`
+
 export const MonthLabel = styled.h2`
-  font-size: 1.5rem;
-  color: #666;
+  font-size: 20px;
+  color: #505050;
+  margin-left: 30px;
 `
 
 export const GlobalStyle = createGlobalStyle`
-
   .fc .fc-daygrid-event-harness {
     display: flex;
     justify-content: center;
@@ -43,7 +101,7 @@ export const GlobalStyle = createGlobalStyle`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-    width: 80%; // 이벤트의 너비를 100%로 설정하여 셀에 맞춤
+    width: 70%;
     height: auto;
     padding: 5px;
     box-sizing: border-box;
@@ -53,21 +111,24 @@ export const GlobalStyle = createGlobalStyle`
     border: none;
   }
 
-
   .fc-daygrid-day-frame {
-    height: 80px; // 셀의 높이를 고정하여 크기 문제 해결
+    height: 80px;
+  }
+
+  .fc .fc-toolbar.fc-header-toolbar {
+    margin-bottom: 0.4rem;
   }
 
   .fc .fc-event-title.fc-sticky {
     color: #505050 !important;
   }
-  
+
   .fc .fc-col-header-cell.fc-day-sun {
-    color: red; /* 일요일은 빨간색 */
+    color: red;
   }
 
   .fc .fc-col-header-cell.fc-day-sat {
-    color: blue; /* 토요일은 파란색 */
+    color: blue;
   }
 
   .fc .fc-col-header-cell.fc-day-mon,
@@ -75,28 +136,26 @@ export const GlobalStyle = createGlobalStyle`
   .fc .fc-col-header-cell.fc-day-wed,
   .fc .fc-col-header-cell.fc-day-thu,
   .fc .fc-col-header-cell.fc-day-fri {
-    color: black; /* 나머지 요일은 검정색 */
+    color: black;
   }
 
-  /* a 태그의 기본 스타일을 재설정하여 요일별 색상이 제대로 적용되도록 함 */
   .fc .fc-col-header-cell.fc-day a {
     color: inherit;
     text-decoration: none;
   }
 
-  /* 날짜 번호 색상 변경 */
   .fc .fc-daygrid-day.fc-day-sun .fc-daygrid-day-number {
-    color: red !important; /* 일요일은 빨간색 */
+    color: red !important;
     font-weight: bold;
   }
 
   .fc .fc-daygrid-day.fc-day-sat .fc-daygrid-day-number {
-    color: blue !important; /* 토요일은 파란색 */
+    color: blue !important;
     font-weight: bold;
   }
 
   .fc .fc-daygrid-day:not(.fc-day-sun):not(.fc-day-sat) .fc-daygrid-day-number {
-    color: black !important; /* 나머지 요일은 검정색 */
+    color: black !important;
     font-weight: medium;
   }
 `
