5d470f8b6d3265ffe2a90323a436ee59990195fd
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-09-06 21:38:37 +0900

    feat: 로그인 정보 로컬스토리지에 저장 구현

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,31 +1,40 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-const useUserStore = create((set) => ({
-  user: {
-    id: '',
-    password: '',
-    profileImage: null,
-    name: '', //유저 네임 추가
-  },
-  // db에 저장되어있는 임의의 사용자 데이터로 가정해놨습니다.
-  dummyUser: {
-    id: 'no2jfamily@naver.com',
-    password: '123123',
-    profileImage: null,
-    name: '정준영',
-  },
-  setUser: (newUser) => set({ user: newUser }),
-  login: () => {
-    set((state) => ({ user: state.user }))
-  },
-  logout: () =>
-    set({
+const useUserStore = create(
+  persist(
+    (set) => ({
       user: {
         id: '',
         password: '',
         profileImage: null,
+        name: '', // 유저 네임 추가
       },
+      dummyUser: {
+        id: 'no2jfamily@naver.com',
+        password: '123123',
+        profileImage: null,
+        name: '정준영',
+      },
+      setUser: (newUser) => set({ user: newUser }),
+      login: () => {
+        set((state) => ({ user: state.dummyUser })) // 로그인하면 dummyUser를 유저로 설정
+      },
+      logout: () =>
+        set({
+          user: {
+            id: '',
+            password: '',
+            profileImage: null,
+            name: '', // 로그아웃 시 유저 정보 초기화
+          },
+        }),
     }),
-}))
+    {
+      name: 'user-storage', // 로컬 스토리지에 저장될 키
+      getStorage: () => localStorage, // 기본적으로 localStorage에 저장되도록 설정
+    },
+  ),
+)
 
 export default useUserStore
